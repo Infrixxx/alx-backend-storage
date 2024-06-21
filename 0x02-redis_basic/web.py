798dc20e2002097185b20a49572ef794c857d771
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2024-06-21 03:35:51 +0200

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,42 @@
 #!/usr/bin/env python3
-""" web.py module """
+""" web.py module 
+"""
+
 
 import requests
 import redis
 from typing import Callable
 from functools import wraps
 
+
 client = redis.Redis()
+
 
 def url_access_count(func: Callable) -> Callable:
     """Tracks how many times a particular URL was accessed and
     cache the result with an expiration time. """
     @wraps(func)
-    def wrapper(url: str) -> str:
-        # Increment the count for the URL access
+    def wrapper(url):
         client.incr(f"count:{url}")
 
-        # Check if the result is cached
         content = client.get(f"result:{url}")
         if content:
             return content.decode('utf-8')
 
-        # Fetch the content if not cached
         content = func(url)
+        client.set(f'count:{url}', 0)
         client.setex(f"result:{url}", 10, content)
 
         return content
-
     return wrapper
+
 
 @url_access_count
 def get_page(url: str) -> str:
-    """obtain the HTML content of a particular URL and returns it."""
+    """obtain the HTML content of a particular URL and returns it.
+    """
+
     response = requests.get(url)
-    return response.text
+    html_content = response.text
+
+    return html_content
